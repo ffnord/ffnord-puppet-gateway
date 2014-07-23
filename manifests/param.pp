d83db947b6d2a43577b52fd12970b41ff212746e
Daniel Ehlers <danielehlers@mindeye.net>
2014-07-23 20:24:15 +0200

    param: Add parameter class
    Collect module parameters in one place, instead of maybe repeating them
    in different places.

---
+++ manifests/param.pp
@@ -0,0 +1,6 @@
+class ffnord::param (
+  $router_id, # This hosts router identifier, e.g. 10.35.0.1
+  $icvpn_as,  # Main AS number of this host, e.g. 65035
+              # This number will be used for the main bird configuration
+) {
+}
