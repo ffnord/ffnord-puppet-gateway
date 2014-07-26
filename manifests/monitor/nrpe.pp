a27a8cfd5da9372e9f14872a0e590daea17d1321
Daniel Ehlers <danielehlers@mindeye.net>
2014-07-26 22:40:27 +0200

    nagios: Add service handling

--- manifests/monitor/nrpe.pp
+++ manifests/monitor/nrpe.pp
@@ -1,7 +1,26 @@
 class ffnord::monitor::nrpe ( $allowed_hosts
                             ) {
-  package { 'nagios-nrpe-server': ensure => installed; 
-            'cron-apt': ensure => installed;
-          }
-  -> file { '/etc/nagios/nrpe_local.cfg': ensure => file, content => template('ffnord/etc/nagios/nrpe_local.cfg.erb'); }
+  package { 
+    'nagios-nrpe-server': 
+      ensure => installed,
+      notify => [Service['nagios-nrpe-server']],
+      require => [File['/etc/nagios/nrpe_local.cfg']];
+    'cron-apt': 
+      ensure => installed;
+  } 
+
+  service {
+    'nagios-nrpe-service':
+       ensure => running,
+       enable => true;
+  }
+
+  file { 
+    '/etc/nagios/nrpe_local.cfg': 
+      ensure => file, 
+      mode => '0644',
+      owner => 'root',
+      group => 'root',
+      content => template('ffnord/etc/nagios/nrpe_local.cfg.erb');
+  }
 }
