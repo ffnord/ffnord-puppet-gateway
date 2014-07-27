9bc4187e190e7cef25935f817d5d7a762cc3d93b
Daniel Ehlers <danielehlers@mindeye.net>
2014-07-27 03:22:09 +0200

    vnstat: Cleanup, refactor ressource dependencies.

--- manifests/monitor/vnstat.pp
+++ manifests/monitor/vnstat.pp
@@ -1,12 +1,22 @@
 class ffnord::monitor::vnstat () {
-  package { 'vnstat' : ensure => installed; }
+  package { 
+    'vnstat': 
+      ensure => installed; 
+  }
+
+  service {
+    'vnstat':
+      ensure  => running,
+      require => [Package['vnstat']];
+  }
 }
 
 define ffnord::monitor::vnstat::device() {
   include ffnord::monitor::vnstat
  
-  Package['vnstat']
-  -> exec { "vnstat device ${name}":
-           command => "/usr/bin/vnstat -u -i ${name}";
-       }
+  exec { "vnstat device ${name}":
+    command => "/usr/bin/vnstat -u -i ${name}",
+    require => [Package['vnstat']],
+    notify => [Service['vnstat']];
+  }
 }
