d7bacf82fd9d7ad72665b95a92f6dfe49ed331cf
Daniel Ehlers <danielehlers@mindeye.net>
2014-08-14 19:38:41 +0200

    network: icmp handling with anon-vpn
    Still missing appropriate firewall forward handling.

--- files/etc/openvpn/anonvpn-up.sh
+++ files/etc/openvpn/anonvpn-up.sh
@@ -2,4 +2,6 @@
 sysctl -w net.ipv4.conf.${1}.rp_filter=0
 ip route replace 0.0.0.0/1 via $4 table 42
 ip route replace 128.0.0.0/1 via $4 table 42
+ip rule del pref 30000 || true
+ip rule add from $4 lookup 42 pref 30000
 exit 0
