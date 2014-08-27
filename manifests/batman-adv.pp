57a3890a2a5bfd9dc19f691e131ff73b6312b6e8
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-08-27 15:03:43 +0200

    batman: add alias for batctl with mesh interface

--- manifests/batman-adv.pp
+++ manifests/batman-adv.pp
@@ -3,10 +3,16 @@ define ffnord::batman-adv( $mesh_code
                          ) {
   include ffnord::resources::batman-adv
 
-  file { 
-    "/etc/network/interfaces.d/${mesh_code}-batman": 
+  file {
+    "/etc/network/interfaces.d/${mesh_code}-batman":
     ensure => file,
     content => template('ffnord/etc/network/mesh-batman.erb'),
     require => [Package['batctl'],Package['batman-adv-dkms']];
+  }
+
+  file_line {
+   'root_bashrc_batffki':
+     path => '/root/.bashrc',
+     line => "alias batctl-ffki='batctl -m bat-${mesh_code}'"
   }
 }
