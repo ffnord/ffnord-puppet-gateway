0f96d8a5ac65f00134a8b9cf3292039c46b5f8cb
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-09-05 11:41:22 +0200

    batman: add firewall chains for traffic on batman interfaces

--- manifests/batman-adv.pp
+++ manifests/batman-adv.pp
@@ -2,6 +2,7 @@ define ffnord::batman-adv( $mesh_code
                          , $batman_it = 5000
                          ) {
   include ffnord::resources::batman-adv
+  include ffnord::firewall
 
   file {
     "/etc/network/interfaces.d/${mesh_code}-batman":
@@ -15,4 +16,31 @@ define ffnord::batman-adv( $mesh_code
      path => '/root/.bashrc',
      line => "alias batctl-${mesh_code}='batctl -m bat-${mesh_code}'"
   }
+
+  # Define Firewall rules for services on bat interfaces, e.g. alfred
+  # introducing the "bat" chain
+  file {
+    '/etc/iptables.d/002-batman-chains':
+     ensure => file,
+     owner => 'root',
+     group => 'root',
+     mode => '0644',
+     content => "ip46tables -N bat-forward\nip46tables -N bat-input",
+     require => [File['/etc/iptables.d/']],
+  }
+
+
+  file {
+    '/etc/iptables.d/900-batman-DROP':
+     ensure => file,
+     owner => 'root',
+     group => 'root',
+     mode => '0644',
+     content => "ip46tables -A bat-forward -j DROP\nip46tables -A bat-input -j DROP",
+     require => [File['/etc/iptables.d/']],
+  }
+
+  ffnord::firewall::device { "bat-${mesh_code}":
+    chain => "bat"
+  } 
 }
