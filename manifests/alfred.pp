014d8bd5b2e22944d30b1de7fe109a9422eed4f9
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-09-12 01:54:31 +0200

    alfred: Extend path

--- manifests/alfred.pp
+++ manifests/alfred.pp
@@ -46,7 +46,7 @@ class ffnord::alfred () {
 
   cron {
    'update-alfred-announce':
-     command => 'PATH=/opt/alfred/:$PATH /opt/alfred-announce/announce.sh',
+     command => 'PATH=/opt/alfred/:/bin:/usr/bin:/sbin:$PATH /opt/alfred-announce/announce.sh',
      user    => root,
      minute  => '*',
      require => [Vcsrepo['/opt/alfred-announce'], Vcsrepo['/opt/alfred']];
