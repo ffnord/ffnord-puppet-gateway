7213daf4ae3a3e016f14a19e9b073cf83e6777fe
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-09-16 00:02:19 +0200

    apt-sources: Add wheezy-backports.

--- manifests/resources/repos.pp
+++ manifests/resources/repos.pp
@@ -1,4 +1,6 @@
-class ffnord::resources::repos () {
+class ffnord::resources::repos (
+  $debian_mirror = $ffnord::params::debian_mirror
+) inherits ffnord::params {
   apt::source { 'repo.universe-factory':
     location   => 'http://repo.universe-factory.net/debian/',
     release    => 'sid',
@@ -15,5 +17,11 @@ class ffnord::resources::repos () {
     key_server  => 'pgpkeys.mit.edu';
   }
 
+  apt::source { 'debian-backports':
+     location          => $debian_mirror,
+     required_packages => 'debian-keyring debian-archive-keyring',
+     release           => 'wheezy-backports',
+     repos             => 'main contrib',
+     include_src       => false,
   }
 }
