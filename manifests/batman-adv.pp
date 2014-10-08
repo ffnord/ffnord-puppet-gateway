fbd890065e4af826b2a7f046a8ff23352ccaf95d
Ralf Heiringhoff <ralf@frosty-geek.net>
2014-10-08 15:03:49 +0200

    fix define ffnord::batman-adv

--- manifests/batman-adv.pp
+++ manifests/batman-adv.pp
@@ -12,32 +12,9 @@ define ffnord::batman-adv( $mesh_code
   }
 
   file_line {
-   'root_bashrc_batffki':
+   "root_bashrc_bat${mesh_code}":
      path => '/root/.bashrc',
      line => "alias batctl-${mesh_code}='batctl -m bat-${mesh_code}'"
-  }
-
-  # Define Firewall rules for services on bat interfaces, e.g. alfred
-  # introducing the "bat" chain
-  file {
-    '/etc/iptables.d/002-batman-chains':
-     ensure => file,
-     owner => 'root',
-     group => 'root',
-     mode => '0644',
-     content => "ip46tables -N bat-forward\nip46tables -N bat-input",
-     require => [File['/etc/iptables.d/']],
-  }
-
-
-  file {
-    '/etc/iptables.d/900-batman-DROP':
-     ensure => file,
-     owner => 'root',
-     group => 'root',
-     mode => '0644',
-     content => "ip46tables -A bat-forward -j DROP\nip46tables -A bat-input -j DROP",
-     require => [File['/etc/iptables.d/']],
   }
 
   ffnord::firewall::device { "bat-${mesh_code}":
