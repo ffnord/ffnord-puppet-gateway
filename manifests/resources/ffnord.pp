ea77d6607cba7a625baadafd8758c7b09795e026
Daniel Ehlers <danielehlers@mindeye.net>
2014-11-04 02:14:57 +0100

    config: New type for defining key-value configuration pairs.

--- manifests/resources/ffnord.pp
+++ manifests/resources/ffnord.pp
@@ -6,3 +6,21 @@ class ffnord::resources::ffnord {
     mode => "0644";
   }
 }
+
+# Define new configuration keys and set a 
+# default value. Because stdlib::file_line 
+# can only match and then replace, but not
+# search and if not exists insert, calling
+# this value will always write the default
+# value.
+define ffnord::resources::ffnord::field(
+  $value = ''
+) { 
+  include ffnord::resources::ffnord
+
+  file_line { "${name}":
+      path => '/etc/ffnord',
+      match => "^${name}=.*",
+      line => "${name}=${value}";
+  }
+}
