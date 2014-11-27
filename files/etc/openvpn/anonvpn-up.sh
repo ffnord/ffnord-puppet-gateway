bd1cf8946249c261c2527974b5645bed761c9faf
ohrensessel <git@leokrueger.de>
2014-11-28 00:37:50 +0100

    routing: remove unreachable defaults from mesh table (42)
    as we always have two rules (lookup mesh and unreachable) one after another,
    the unreachable default route in the mesh table (42) shouln't be needed anymore

--- files/etc/openvpn/anonvpn-up.sh
+++ files/etc/openvpn/anonvpn-up.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sysctl -w net.ipv4.conf.${1}.rp_filter=0
-ip route replace 0.0.0.0/1 via $4 table 42
-ip route replace 128.0.0.0/1 via $4 table 42
+ip route replace default via $4 table 42
 ip rule del pref 30000 || true
 ip rule add from $4 lookup 42 pref 30000
 exit 0
