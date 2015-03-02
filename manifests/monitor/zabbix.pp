72b10e95c472f1cdf6be6254bdab2f20b99067c1
ohrensessel <git@ohrensessel.net>
2015-03-02 22:41:57 +0100

    zabbix: use \n as newline

--- manifests/monitor/zabbix.pp
+++ manifests/monitor/zabbix.pp
@@ -78,8 +78,7 @@ define ffnord::monitor::zabbix::check_script (
         'mode' => '0440',
         'owner' => 'root',
         'group' => 'root',
-        'content' => inline_template("zabbix ALL= NOPASSWD: /opt/bin/zabbix/${scriptname}.sh
-"),
+        'content' => inline_template("zabbix ALL= NOPASSWD: /opt/bin/zabbix/${scriptname}.sh\n"),
       })
 
     } else {
