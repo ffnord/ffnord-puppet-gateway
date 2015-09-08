cd4f35406a8f9908981624900317bdb237624e25
ruben <ruben.git@r.z11.de>
2015-09-08 12:48:49 +0200

    Configurable fastd connection attempts #133

--- manifests/fastd.pp
+++ manifests/fastd.pp
@@ -8,6 +8,7 @@ define ffnord::fastd( $mesh_code
                      , $fastd_port
 
                      , $fastd_peers_git
+                     , $fastd_verify = ''
                      ) {
   #validate_re($mesh_mac, '^de:ad:be:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}$')
 
@@ -21,27 +22,27 @@ define ffnord::fastd( $mesh_code
 
   ffnord::monitor::zabbix::check_script {
     "${mesh_interface}_fastdcons":
-      mesh_code => $mesh_code,
+      mesh_code  => $mesh_code,
       scriptname => "fastd_connections",
-      sudo => true;
+      sudo       => true;
     "${mesh_interface}_fastdcons6":
-      mesh_code => $mesh_code,
+      mesh_code  => $mesh_code,
       scriptname => "fastd_connections6",
-      sudo => true;
+      sudo       => true;
   }
 
   file {
     "/etc/fastd/${mesh_interface}-mvpn/":
-      ensure =>directory,
+             ensure  =>directory,
              require => Package[ffnord::resources::fastd];
     "/etc/fastd/${mesh_interface}-mvpn/fastd.conf":
-      ensure => file,
-             notify => Service[ffnord::resources::fastd],
+             ensure  => file,
+             notify  => Service[ffnord::resources::fastd],
              content => template('ffnord/etc/fastd/fastd.conf.erb');
     "/etc/fastd/${mesh_interface}-mvpn/secret.conf":
       ensure => file,
       source => $fastd_secret,
-      mode => '0600',
+      mode   => '0600',
   }
   if ! defined(Ffnord::Batman-Adv["ffnord_batman_adv_${mesh_code}"]) {
       ffnord::batman-adv { "ffnord_batman_adv_${mesh_code}":
@@ -63,10 +64,10 @@ define ffnord::fastd( $mesh_code
 
   file {
     "/etc/fastd/${mesh_interface}-mvpn/peers/.git/hooks/post-merge":
-       ensure => file,
-       owner => 'root',
-       group => 'root',
-       mode => '0755',
+       ensure  => file,
+       owner   => 'root',
+       group   => 'root',
+       mode    => '0755',
        content => "#!/bin/sh\n/usr/local/bin/update-fastd-keys reload",
        require => Vcsrepo["/etc/fastd/${mesh_interface}-mvpn/peers"];
   }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,22 @@
 define ffnord::mesh(
-  $mesh_name,        # Name of your community, e.g.: Freifunk Gotham City
-  $mesh_code,        # Code of your community, e.g.: ffgc
-  $mesh_as,          # AS of your community
-  $mesh_mac,         # mac address mesh device: 52:54:00:bd:e6:d4
-  $vpn_mac,          # mac address vpn device, ideally != mesh_mac and unique
-  $mesh_mtu = 1280,  # mtu used, default only suitable for fastd via ipv4
-  $range_ipv4,       # ipv4 range allocated to community in cidr notation, e.g. 10.35.0.1/16
-  $mesh_ipv4,        # ipv4 address in cidr notation, e.g. 10.35.0.1/19
-  $mesh_ipv6,        # ipv6 address in cidr notation, e.g. fd35:f308:a922::ff00/64
-  $mesh_peerings,    # path to the local peerings description yaml file
+  $mesh_name,         # Name of your community, e.g. 'Freifunk Gotham City'
+  $mesh_code,         # Code of your community, e.g. 'ffgc'
+  $mesh_as,           # AS of your community
+  $mesh_mac,          # mac address mesh device: e.g. '52:54:00:bd:e6:d4'
+  $vpn_mac,           # mac address vpn device, ideally != mesh_mac and unique
+  $mesh_mtu = 1280,   # mtu used, default only suitable for fastd via ipv4
+  $range_ipv4,        # ipv4 range allocated to community in cidr notation, e.g. '10.35.0.1/16'
+  $mesh_ipv4,         # ipv4 address in cidr notation, e.g. '10.35.0.1/19'
+  $mesh_ipv6,         # ipv6 address in cidr notation, e.g. 'fd35:f308:a922::ff00/64'
+  $mesh_peerings,     # path to the local peerings description yaml file
 
-  $fastd_peers_git,  # fastd peers
-  $fastd_secret,     # fastd secret
-  $fastd_port,       # fastd port
+  $fastd_peers_git,   # fastd peers
+  $fastd_secret,      # fastd secret
+  $fastd_port,        # fastd port
+  $fastd_verify = '', # fastd verification override string for connection attempts, e.g. 'true' = accept all, '' = default (no override)
 
-  $dhcp_ranges = [], # dhcp pool
-  $dns_servers = [], # other dns servers in your network
+  $dhcp_ranges = [],  # dhcp pool
+  $dns_servers = [],  # other dns servers in your network
 ) {
 
   # TODO We should handle parameters in a param class pattern.
@@ -63,14 +64,15 @@ define ffnord::mesh(
     dns_servers  => $dns_servers;
   } ->
   ffnord::fastd { "fastd_${mesh_code}":
-    mesh_code => $mesh_code,
-    mesh_interface => "${mesh_code}",
-    mesh_mac  => $mesh_mac,
-    vpn_mac   => $vpn_mac,
-    mesh_mtu  => $mesh_mtu,
-    fastd_secret => $fastd_secret,
-    fastd_port   => $fastd_port,
-    fastd_peers_git => $fastd_peers_git;
+    mesh_code       => $mesh_code,
+    mesh_interface  => "${mesh_code}",
+    mesh_mac        => $mesh_mac,
+    vpn_mac         => $vpn_mac,
+    mesh_mtu        => $mesh_mtu,
+    fastd_secret    => $fastd_secret,
+    fastd_port      => $fastd_port,
+    fastd_peers_git => $fastd_peers_git,
+    fastd_verify    => $fastd_verify;
   } ->
   ffnord::radvd { "br-${mesh_code}":
     mesh_ipv6_address    => $mesh_ipv6_address,
@@ -92,25 +94,25 @@ define ffnord::mesh(
 
   if $ffnord::params::include_bird6 {
     ffnord::bird6::mesh { "bird6-${mesh_code}":
-      mesh_code => $mesh_code,
-      mesh_ipv4_address => $mesh_ipv4_address,
-      mesh_ipv6_address => $mesh_ipv6_address,
-      mesh_peerings => $mesh_peerings,
-      site_ipv6_prefix => $mesh_ipv6_prefix,
+      mesh_code           => $mesh_code,
+      mesh_ipv4_address   => $mesh_ipv4_address,
+      mesh_ipv6_address   => $mesh_ipv6_address,
+      mesh_peerings       => $mesh_peerings,
+      site_ipv6_prefix    => $mesh_ipv6_prefix,
       site_ipv6_prefixlen => $mesh_ipv6_prefixlen,
-      icvpn_as => $mesh_as;
+      icvpn_as            => $mesh_as;
     }
   }
   if $ffnord::params::include_bird4 {
     ffnord::bird4::mesh { "bird4-${mesh_code}":
-      mesh_code => $mesh_code,
-      mesh_ipv4_address => $mesh_ipv4_address,
-      range_ipv4 => $range_ipv4,
-      mesh_ipv6_address => $mesh_ipv6_address,
-      mesh_peerings => $mesh_peerings,
-      site_ipv4_prefix => $mesh_ipv4_prefix,
+      mesh_code           => $mesh_code,
+      mesh_ipv4_address   => $mesh_ipv4_address,
+      range_ipv4          => $range_ipv4,
+      mesh_ipv6_address   => $mesh_ipv6_address,
+      mesh_peerings       => $mesh_peerings,
+      site_ipv4_prefix    => $mesh_ipv4_prefix,
       site_ipv4_prefixlen => $mesh_ipv4_prefixlen,
-      icvpn_as => $mesh_as;
+      icvpn_as            => $mesh_as;
     }
   }
  
