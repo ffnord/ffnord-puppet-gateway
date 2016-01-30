3c0d3ec5a8d063ec895cb8840096b5d0c85f24ee
rubo77 <github@r.z11.de>
2016-01-30 20:42:08 +0100

    Install apt-transport-https before fetching fastd sources from https://repo.universe-factory.net/

--- manifests/resources/repos.pp
+++ manifests/resources/repos.pp
@@ -1,15 +1,19 @@
 class ffnord::resources::repos (
   $debian_mirror = $ffnord::params::debian_mirror
 ) inherits ffnord::params {
+  package { 
+    'apt-transport-https':
+      ensure => installed;
+  } ->
   apt::source { 'repo.universe-factory':
-    location   => 'http://repo.universe-factory.net/debian/',
+    location   => 'https://repo.universe-factory.net/debian/',
     release    => 'sid',
     repos      => 'main',
     key        => '16EF3F64CB201D9C',
     key_server => 'pgpkeys.mit.edu';
   }
 
- apt::source { 'debian.draic.info':
+  apt::source { 'debian.draic.info':
     location    => 'http://debian.draic.info/',
     release     => "${lsbdistcodename}",
     repos       => 'main',
