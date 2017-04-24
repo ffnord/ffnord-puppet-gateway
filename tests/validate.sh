706fee5a0bffd2746e70411d071018edc3f6a126
rubo77 <github@r.z11.de>
2017-04-25 01:22:06 +0200

    --no-parameter_order-check

--- tests/validate.sh
+++ tests/validate.sh
@@ -8,5 +8,5 @@ fi
 for manifest in $*;do
   echo "Validiere $manifest"
   puppet parser validate $manifest
-  puppet-lint --no-arrow_on_right_operand_line-check --no-names_containing_dash-check --no-documentation-check --no-arrow_alignment-check --no-140chars-check --no-autoloader_layout-check --with-filename $manifest
+  puppet-lint --no-arrow_on_right_operand_line-check --no-parameter_order-check --no-names_containing_dash-check --no-documentation-check --no-arrow_alignment-check --no-140chars-check --no-autoloader_layout-check --with-filename $manifest
 done
